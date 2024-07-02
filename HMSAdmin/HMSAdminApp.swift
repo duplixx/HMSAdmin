e5b9182a74841033bb3195bf93aeb10235d7189e
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2024-07-02 11:49:59 +0530

    Initial Commit

---
+++ HMSAdmin/HMSAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMSAdminApp.swift
+//  HMSAdmin
+//
+//  Created by pushker yadav on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HMSAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
